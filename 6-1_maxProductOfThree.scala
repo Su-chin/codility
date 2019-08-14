77166fe382cbac38edbf56ff957ab21e6469abb4
sumiya.kogo <lionboy0043@gmail.com>
2019-08-14 22:16:28 +0900

    finished 6-1

--- 6-1_maxProductOfThree.scala
+++ 6-1_maxProductOfThree.scala
@@ -1,8 +1,30 @@
+/*
+正数が３つ
+or
+正数が１つ and 負の数が２つ
+負の整数が２つの場合は許容できる
+ */
+
 object Solution {
   def solution (a: Array[Int]): Int = {
     val sortedArray = a.sorted
-    sortedArray(sortedArray.length) * sortedArray(sortedArray.length - 1) * sortedArray(sortedArray.length - 2)
+    val pattern1 = sortedArray(sortedArray.length - 1) * sortedArray(sortedArray.length - 2) * sortedArray(sortedArray.length - 3)
+    val pattern2 = sortedArray(0) * sortedArray(1) * sortedArray(sortedArray.length - 1)
+    if (pattern1 > pattern2) {
+      pattern1
+    } else {
+      pattern2
+    }
+  }
+
+  def solution2 (a: Array[Int]): Int = {
+    val sortedArray = a.sorted
+    val end = sortedArray.length - 1
+    Math.max(
+      sortedArray(0) * sortedArray(1) * sortedArray(end),
+      sortedArray(end - 2) * sortedArray(end - 1) * sortedArray(end)
+    )
   }
 }
 
-Solution.solution(Array(-3,1,2,-2,5,6))
+Solution.solution2(Array(-3,1,2,-2,5,6))
