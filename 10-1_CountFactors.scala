3f211e8a2b1de8c6a4d167b85d0f363fb6939a7a
sumiya.kogo <sumiya.kogo@nextbeat.net>
2019-09-17 13:52:40 +0900

    2乗する意味とは？

---
+++ 10-1_CountFactors.scala
@@ -0,0 +1,23 @@
+object Solution {
+  def solution(n: Int):Int = {
+    var i: Long = 1
+    var count = 0
+    while(i * i < n) {
+      // 24
+      // i = 1,2,4,16(4回)
+      if(n % i == 0) {
+        // n/i = 24,12,8,6
+        count += 2
+        // i = 1,2,3,4
+      }
+      i += 1
+    }
+    if(i*i == n) {
+      count + 1
+    } else {
+      count
+    }
+  }
+}
+
+Solution.solution(24)
